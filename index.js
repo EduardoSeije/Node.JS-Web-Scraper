3a5f0fe0db332ce4ccb8859b4f8c366ab538c949
Eduardo Seije <eduardoseije@gmail.com>
2021-10-21 18:53:32 -0300

    Configuração do axios para a url desejada

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ const express = require('express');
 
 const app = express();
 
+const URL = 'https://www.uol.com.br/';
+
+axios(URL)
+  .then(response => {
+    const html = response.data;
+    console.log(html);
+  })
+
+
 app.listen(PORT, () => console.log(`Server working on ${PORT}`));
